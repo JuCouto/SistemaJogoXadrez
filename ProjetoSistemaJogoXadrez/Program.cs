47a4d2178cb5e98550b0126a8f318e82e7a7cf27
Ju Couto <jnalmeida@ymail.com>
2022-10-14 22:21:50 -0300

    Colocando Peças no Tabuleiro

--- ProjetoSistemaJogoXadrez/Program.cs
+++ ProjetoSistemaJogoXadrez/Program.cs
@@ -1,9 +1,13 @@
 ﻿
 using ProjetoSistemaJogoXadrez;
 using tabuleiro;
+using Xadrez;
 
 
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
+
 Tela.ImprimirTabuleiro(tab);
 Console.ReadLine();

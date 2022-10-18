9d339a8de6f53e5954808bd83b661da1cbded493
Ju Couto <jnalmeida@ymail.com>
2022-10-18 12:01:03 -0300

    Criando classe Melhorando classe Imprimir Peça, acrescentando cor

--- ProjetoSistemaJogoXadrez/Program.cs
+++ ProjetoSistemaJogoXadrez/Program.cs
@@ -7,7 +7,7 @@ using Xadrez;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
+tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 5));
 
 Tela.ImprimirTabuleiro(tab);
 Console.ReadLine();
